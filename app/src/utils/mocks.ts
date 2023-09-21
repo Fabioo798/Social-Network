761632a1a51da47473447b4423f680ebbe95e506
Fabio <fabiodn.798@gmail.com>
2023-09-21 21:47:03 +0200

    ✅ Test Thunks

---
+++ app/src/utils/mocks.ts
@@ -0,0 +1,8 @@
+import { UserStructure } from "../users/model/user.model";
+
+export const mockLoginUser: Partial<UserStructure> = {
+ email: "test@test",
+ password: "test",
+};
+
+export const token: string = "test";
